67c0b1c3d0cdb53d47fac0da8e012114188ec47b
boyar-dee <charlie.yin02@gmail.com>
2026-02-19 22:54:44 -0800

    Code Created

---
+++ 2026-02-18/CCC '14 J2 - Vote Count.py
@@ -0,0 +1,12 @@
+n = int(input().strip())
+votes = input().strip()
+
+a = votes.count("A")
+b = votes.count("B")
+
+if a > b:
+    print("A")
+elif b > a:
+    print("B")
+else:
+    print("Tie")
---
+++ 2026-02-18/CCC '16 J1 - Tournament Selection.py
@@ -0,0 +1,15 @@
+wins = 0
+
+for i in range(6):
+    result = input().strip()
+    if result == 'W':
+        wins += 1
+
+if wins >= 5:
+    print(1)
+elif wins >= 3:
+    print(2)
+elif wins >= 1:
+    print(3)
+else:
+    print(-1)
